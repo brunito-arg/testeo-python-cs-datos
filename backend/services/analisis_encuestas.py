1a1cc2209572d38bea561dfb2e5f41c366bd0481
Bruno <brunomassacane@gmail.com>
2026-06-07 17:57:37 -0300

    COMENTARIOS
    agregue comentarios

--- backend/services/analisis_encuestas.py
+++ backend/services/analisis_encuestas.py
@@ -1,3 +1,6 @@
+# ANALISIS ENCUESTAS LIMPIO INFO DE LOS FORMS
+
+
 import pandas as pd
 
 URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQuM2Y9QqcH_1LIkmdHHflDYTK7y3u9jgUlmZ-XYzaky7HYNyN3_DceBNSagYXESgdYZgnKSRIv5CkU/pub?gid=1237250350&single=true&output=csv"
